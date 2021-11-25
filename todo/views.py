3cd9cae5a3f5ab408320b8ca53d7a84b5dc44135
Lukasz Hoszowski <hoszak@gmail.com>
2021-11-25 19:51:32 +0100

    task_view

--- todo/views.py
+++ todo/views.py
@@ -7,4 +7,9 @@ class TaskView(CreateView):
     model = Task
     fields = '__all__'
     template_name = 'todo/todo.html'
-    # success_url = reverse_lazy('todo:')
+    success_url = reverse_lazy('todo:todo')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task_list'] = Task.objects.filter(is_done=False, archive=False)
+        return context
